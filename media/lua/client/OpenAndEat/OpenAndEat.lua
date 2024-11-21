03ccfcaf77ed5d5de1e472f26c2c39b436183ac0
Duncan MacDonald <buzzbuzz.macdonald@gmail.com>
2024-11-21 01:51:05 -0800

    More stuff

--- media/lua/client/OpenAndEat/OpenAndEat.lua
+++ media/lua/client/OpenAndEat/OpenAndEat.lua
@@ -1,54 +1,76 @@
 local OpenAndEat = {}
 
+function OpenAndEat.OnOpenAndEat(player, cannedItem)
+	print("Canned item:".. cannedItem:getName())
+	-- Get type of opened canned food item
+	local openedFoodType = cannedItem:getReplaceOnUse() or cannedItem:getReplaceOnUseOn() or cannedItem:getReplaceOnUseFullType()
+
+	-- Replace canned food with opened food
+	if openedFoodType then
+		player:getInventory():Remove(item)
+		local openedFoodItem = InventoryItemFactory.CreateItem(openedFoodType)
+		player:getInventory():AddItem(openedFoodItem)
+		print("Added opened item to inventory")
+		
+		-- Eat opened food
+		ISInventoryPaneContextMenu.eatItem(openedFoodItem, 1, player)
+	else
+		print("No openedFoodType")
+		return
+	end
+end
+
 function OpenAndEat.OnFillInventoryObjectContextMenu(playerIndex, contextMenu, clickedItems)
 	local playerObj = getSpecificPlayer(playerIndex)
 	
 	print("---------------------------------")
 	print("Mod started")
 	
-	-- Ensure player is alive before doing anything. idk if this is needed
-	if not playerObj or playerObj:isDead() then
-		print("Player is dead or something, idk")
-		return
-	end
-	
-	-- Figure out if clickedItem is a stack or a single item
-	local item = clickedItems
-	if not instanceof(item, "InventoryItem") then
-		item = tmpItem.items[1]
-	end
-	
 	-- Check for can opener
-	local canOpener = invItems:getFirstTypeRecurse("Base.CanOpener")
+	local canOpener = playerObj:getInventory():FindAndReturn("Base.TinOpener")
 	if not canOpener then
 		print("Can't seem to find a can opener")
 		return
 	end
 	
-	-- Check if item is canned food
-	local itemName = item:getName()
-	if not string.find(string.lower(itemName), "canned") then
-		print("Item does not contain word 'canned': ", itemName)
-		return
-	end
-	print("Canned food: ", itemName)
-	
-	-- Get type of opened canned food item
-	local openedFoodType = item:getReplaceOnUse()
-	
-	-- Replace canned food with opened food
-	if openedFoodType then
-		playerObj:getInventory():Remove(item)
-		local openedFoodItem = InventoryItemFactory.CreateItem(openedFoodType)
-		playerObj:getInventory():AddItem(openedFoodItem)
-		print("Added opened item to inventory")
+	-- Check if item is a canned food item
+	local cannedItem = nil
+	for _, clickedItem in ipairs(clickedItems) do
+		local item = clickedItem
+		if not instanceof(clickedItem, "InventoryItem") then
+			item = clickedItem.items[1]
+		end
 		
-		-- Eat opened food
-		ISInventoryPaneContextMenu.eatItem(openedFoodItem, 1, playerObj)
-	else
-		print("No openedFoodType")
-		return
+--		if string.find(string.lower(item:getFullType()), "canned") then
+		if item:getStringItemType() == "CannedFood" then
+			cannedItem = item
+		end
 	end
+	
+	if cannedItem then 
+		contextMenu:addOption("Open and eat", playerObj, OpenAndEat.OnOpenAndEat, cannedItem)
+	end
+	
+--	Check if item is canned food
+--	local itemName = item:getName()
+	
+	
+--	-- Get type of opened canned food item
+--	local openedFoodType = item:getReplaceOnUse()
+--	
+--	-- Replace canned food with opened food
+--	if openedFoodType then
+--		playerObj:getInventory():Remove(item)
+--		local openedFoodItem = InventoryItemFactory.CreateItem(openedFoodType)
+--		playerObj:getInventory():AddItem(openedFoodItem)
+--		print("Added opened item to inventory")
+--		
+--		-- Eat opened food
+--		ISInventoryPaneContextMenu.eatItem(openedFoodItem, 1, playerObj)
+--	else
+--		print("No openedFoodType")
+--		return
+--	end
 end
 
 Events.OnFillInventoryObjectContextMenu.Add(OpenAndEat.OnFillInventoryObjectContextMenu)
