14a7a8806189dab3026919a7c91ad831f9c030f9
Duncan MacDonald <buzzbuzz.macdonald@gmail.com>
2024-11-22 20:38:42 -0800

    Don't show tooltip on opened food

--- media/lua/client/OpenAndEat/OpenAndEat.lua
+++ media/lua/client/OpenAndEat/OpenAndEat.lua
@@ -82,14 +82,7 @@ function OpenAndEat.OnFillInventoryObjectContextMenu(playerIndex, contextMenu, c
 	print("---------------------------------")
 	print("Mod started")
 
-	-- Check for can opener
-	local canOpener = playerObj:getInventory():FindAndReturn("Base.TinOpener")
-	if not canOpener then
-		print("Can't seem to find a can opener")
-		return
-	end
-	
-	-- Check if item is a canned food item
+	-- Check if item is an unopened canned food item
 	local cannedItem = nil
 	for _, clickedItem in ipairs(clickedItems) do
 		local item = clickedItem
@@ -97,15 +90,15 @@ function OpenAndEat.OnFillInventoryObjectContextMenu(playerIndex, contextMenu, c
 			item = clickedItem.items[1]
 		end
 		
---		if string.find(string.lower(item:getFullType()), "canned") then
-		if item:getStringItemType() == "CannedFood" then
+		print("Item type:", item:getType())
+		if item:getStringItemType() == "CannedFood" and not string.find(item:getType(), "Open") then
 			cannedItem = item
 		end
 	end
-	
-	if cannedItem then 
-		contextMenu:addOption("Open and eat", playerObj, OpenAndEat.OnOpenAndEat, cannedItem)
-	end
+
+	if not cannedItem then return end
+
+	contextMenu:addOption("Open and eat", playerObj, OpenAndEat.OnOpenAndEat, cannedItem)
 end
 
 Events.OnFillInventoryObjectContextMenu.Add(OpenAndEat.OnFillInventoryObjectContextMenu)
